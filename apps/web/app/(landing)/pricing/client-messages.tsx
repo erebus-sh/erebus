08b9578f79e98148f825ef3c6011a8492472a78b
#V0ID <hey@v0id.me>
2025-09-15 19:49:43 +0300

    fix(pricing): add delay before showing toast notification for expired subscriptions to ensure proper rendering

--- apps/web/app/(landing)/pricing/client-messages.tsx
+++ apps/web/app/(landing)/pricing/client-messages.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useQueryState } from "nuqs";
 import { toast } from "sonner";
 import { useEffect } from "react";
@@ -6,12 +8,22 @@ export const MessagesClientSide = () => {
   const [expired, setExpired] = useQueryState("expired");
 
   useEffect(() => {
-    if (expired) {
-      toast.error(
-        "Your subscription has expired. Please renew your subscription to continue.",
-      );
-      setExpired(null);
-    }
+    (async () => {
+      /*
+       *I hate react, I swear to god man WTF
+       * I need this because for some reason, the toast is not
+       * showing up if you immediately call it, because I guess
+       * some SSR render pipeline BS, bruhhhhhh!
+       *
+       */
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      if (expired) {
+        toast.error(
+          "Your subscription has expired. Please renew your subscription to continue.",
+        );
+        setExpired(null);
+      }
+    })();
   }, [expired, setExpired]);
 
   return <></>;
