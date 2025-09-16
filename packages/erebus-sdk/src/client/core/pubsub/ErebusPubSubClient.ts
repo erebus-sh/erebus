2dd71783a319a5a5b3d28712bd168a574e7144fe
#V0ID <hey@v0id.me>
2025-09-16 21:54:10 +0300

    refactor(pubsub): removing optional error handling parameter

--- packages/erebus-sdk/src/client/core/pubsub/ErebusPubSubClient.ts
+++ packages/erebus-sdk/src/client/core/pubsub/ErebusPubSubClient.ts
@@ -91,7 +91,7 @@ export class ErebusPubSubClientNew {
    * Join a channel for this client instance
    * @param channel The channel to join
    */
-  joinChannel(channel: string, throwError: boolean = true): void {
+  joinChannel(channel: string): void {
     const instanceId = this.#conn.connectionId;
     logger.info(`[Erebus:${instanceId}] Joining channel`, { channel });
     logger.info("Erebus.joinChannel() called", { channel });
@@ -104,11 +104,6 @@ export class ErebusPubSubClientNew {
       const error = "Invalid channel: must be a non-empty string";
       logger.error(`[Erebus:${instanceId}] ${error}`, { channel });
       logger.error("Invalid channel", { channel });
-      if (throwError) {
-        // Treat it as an error
-        throw new Error(error);
-      }
-      // if not throwing, just return
       return;
     }
 
